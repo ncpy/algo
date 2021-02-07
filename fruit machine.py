c056ba4af691fc8276864dde2c71513726e7d3d7
ncpy <ncpy@github.com>
2021-02-07 07:44:18 +0300

    last step

--- fruit machine.py
+++ fruit machine.py
@@ -1,12 +1,19 @@
 import random
 
-money_in_machine = 100
+money_in_machine = 1000
 user_credit = 50
-play_credit = 10
+play_credit = 20
 no_of_free_play = 0
 constant = 1
 symbols = ["black", "white", "green", "yellow"]
 no_of_slots = 4
+
+def check_difference(slots):
+    for i in range(no_of_slots):
+        for j in range(i+1, no_of_slots):
+            if slots[i] == slots[j]:    # except (all diff and all same)
+                return False
+    return True
 
 def play(user_credit, money_in_machine, no_of_free_play):
     slots = []
@@ -14,24 +21,18 @@ def play(user_credit, money_in_machine, no_of_free_play):
         slots.append(random.choice(symbols))
     print(slots)
 
-
     if slots[0] == slots[1] == slots[2] == slots[3]:        # all is equal
          user_credit = user_credit + money_in_machine
          money_in_machine = 0
-         return user_credit, money_in_machine
+         return user_credit, money_in_machine, no_of_free_play
 
-    def check_difference():
-        for i in range(no_of_slots):
-            for j in range(i+1, no_of_slots):
-                if slots[i] == slots[j]:    # except (all diff and all same)
-                    return False
-        return True
-
-    if check_difference() == True:    #if all is diff. which means check_difference() func. return TRUE
+    if check_difference(slots) == True:    #if all is diff. which means check_difference() func. return TRUE
         user_credit = user_credit + money_in_machine/2
         money_in_machine = money_in_machine/2
-        return user_credit, money_in_machine
-    elif check_difference() == False:
+        return user_credit, money_in_machine, no_of_free_play
+
+    # 2 or more adjacent slots are equal
+    if slots[0] == slots[1] or slots[1] == slots[2] or slots[2] == slots[3] or slots[0] == slots[1] == slots[2] or slots[1] == slots[2] == slots[3]:
         if money_in_machine >= 5*play_credit:
             user_credit = user_credit + 5*play_credit
             money_in_machine = money_in_machine - 5*play_credit
@@ -39,16 +40,12 @@ def play(user_credit, money_in_machine, no_of_free_play):
             no_of_free_play = no_of_free_play + int((5*play_credit - money_in_machine)/play_credit)
             user_credit = user_credit + money_in_machine
             money_in_machine = 0
+        return user_credit, money_in_machine, no_of_free_play
             
-            pass
-
-        return user_credit, money_in_machine
-            
-
-    return user_credit, money_in_machine
+    return user_credit, money_in_machine, no_of_free_play
 
 #while True:
-for i in range(20):
+for i in range(100):
     if no_of_free_play > 0:
         constant = 0
         no_of_free_play -= 1
@@ -58,12 +55,13 @@ for i in range(20):
     if user_credit >= play_credit*constant:
         print("\nbefore playing: ",user_credit, money_in_machine)
 
-        user_credit = user_credit - play_credit
-        money_in_machine = money_in_machine + play_credit
+        user_credit = user_credit - play_credit*constant
+        money_in_machine = money_in_machine + play_credit*constant
         print("playing..: ",user_credit, money_in_machine)
 
-        user_credit, money_in_machine = play(user_credit, money_in_machine, no_of_free_play)
+        user_credit, money_in_machine, no_of_free_play = play(user_credit, money_in_machine, no_of_free_play)
         print("after playing: ",user_credit, money_in_machine)
+        print("free ", no_of_free_play)
     else:
         print("User dont have enough money to play")
         break
