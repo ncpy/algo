f08e008a31139851a443a938513ba9896370afc7
necip <43618473+ncpy@users.noreply.github.com>
2021-02-08 17:48:18 +0300

    Update split_the_treasure.py
    last

--- split_the_treasure.py
+++ split_the_treasure.py
@@ -4,16 +4,15 @@ treasure = [27,7,20]   #[3,3,3,3,2,2,2,2,2,2,2,2]  #[6,3,2,4,1] #[4,4,4]  #[27,7
 total = sum(treasure)
 print("total: ", total)
 
+def solve(each):
+    for j in range(len(treasure)):
+        for k in itertools.combinations(treasure, j):
+            k = list(k)
+            if sum(k) == each:
+                print(sum(k) ," each ", k, " among ", i, "persons")
+
 for i in range(2, len(treasure)+1):
     each = total/i
     if each %1 == 0:
         each = int(each)
-        print()
-        for j in range(len(treasure)):
-            for k in itertools.combinations(treasure, j):
-                k = list(k)
-                if sum(k) == each:
-                    print(sum(k) ," each ", k, " among ", i, "persons")
-
-                #else: print("it can't be split")
-                    
+        solve(each)
