f97e5b90d7e1a630d1939c68f0bba49d54245795
necip <43618473+ncpy@users.noreply.github.com>
2021-02-08 17:44:13 +0300

    Add files via upload
    new one

--- split_the_treasure.py
+++ split_the_treasure.py
@@ -1,32 +1,19 @@
+import itertools
 
-#given
-crew = 3
+treasure = [27,7,20]   #[3,3,3,3,2,2,2,2,2,2,2,2]  #[6,3,2,4,1] #[4,4,4]  #[27,7,20] --> (20,7)+(27)
+total = sum(treasure)
+print("total: ", total)
 
-treasure = [27,7,20]  #[4,4,4]  #[27,7,20] --> (20,7)+(27)
+for i in range(2, len(treasure)+1):
+    each = total/i
+    if each %1 == 0:
+        each = int(each)
+        print()
+        for j in range(len(treasure)):
+            for k in itertools.combinations(treasure, j):
+                k = list(k)
+                if sum(k) == each:
+                    print(sum(k) ," each ", k, " among ", i, "persons")
 
-
-def subset_sum(numbers, target, partial=[]):
-    s = sum(partial)
-
-    # check if the partial sum is equals to target
-    if s == target:
-        print("sum(%s)=%s" % (partial, target))
-    if s >= target:
-        return  # if we reach the number why bother to continue
-
-    for i in range(len(numbers)):
-        n = numbers[i]
-        remaining = numbers[i + 1:]
-        subset_sum(remaining, target, partial + [n])
-
-
-t = 0
-for i in treasure:
-    t += i
-print("tplm: ",t)
-each = t/2
-if each%1==0:
-    each = int(each)
-    print("each: ",each)
-    subset_sum(treasure, each)   # topla=16, 2,3,4 ve 5 e böl -->8,x,4
-
+                #else: print("it can't be split")
+                    
